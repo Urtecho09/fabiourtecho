141b4943d446c7d0b42ce35100daa198ccdc5136
Urtecho09 <135347499+Urtecho09@users.noreply.github.com>
2023-06-07 16:56:41 -0600

    Add files via upload

---
+++ lab1/app.js
@@ -0,0 +1,17 @@
+const edad = document.getElementById("entrada");
+const boton= document.getElementById("agregar");
+const respuesta = document.getElementById("respuesta");
+
+function mensaje(){
+    const nac = 2023-parseInt(edad.value);
+    respuesta.innerHTML = "Su año de nacimiento es: " + nac;
+    edad.value="";
+}
+
+edad.addEventListener("keydown",(event)=>{
+    if (event.key==="Enter"){
+        mensaje();
+    }
+});
+
+boton.addEventListener("click", mensaje);
